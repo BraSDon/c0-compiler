91237bbda5404cb683f2cf378da7421bc4793e17
Brandon Schuehlein <brandon@schuehlein.eu>
2025-05-26 10:00:42 +0200

    feat: interference graph + coloring

---
+++ src/main/java/edu/kit/kastel/vads/compiler/backend/x86/RegisterRequirement.java
@@ -0,0 +1,18 @@
+package edu.kit.kastel.vads.compiler.backend.x86;
+
+import edu.kit.kastel.vads.compiler.ir.node.Node;
+import edu.kit.kastel.vads.compiler.ir.node.ProjNode;
+import edu.kit.kastel.vads.compiler.ir.node.ReturnNode;
+import edu.kit.kastel.vads.compiler.ir.node.ConstIntNode;
+import edu.kit.kastel.vads.compiler.ir.node.StartNode;
+import edu.kit.kastel.vads.compiler.ir.node.Block;
+
+public final class RegisterRequirement {
+    public static boolean needsRegister(Node node) {
+        return (node instanceof ProjNode ||
+                node instanceof ReturnNode ||
+                node instanceof ConstIntNode ||
+                node instanceof StartNode ||
+                node instanceof Block);
+    }
+}
